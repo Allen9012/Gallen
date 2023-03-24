68562f8fd88d540ef7bddb2dedf2aa03315d889c
Allen <2439506288@qq.com>
2023-03-24 21:31:42 +0800

    gellen web 0.4 完成template

---
+++ template/gellen/logger.go
@@ -0,0 +1,22 @@
+/**
+  @author: Allen
+  @since: 2023/3/24
+  @desc: //TODO
+**/
+package gellen
+
+import (
+	"log"
+	"time"
+)
+
+func Logger() HandlerFunc {
+	return func(c *Context) {
+		// Start timer
+		t := time.Now()
+		// process request
+		c.Next()
+		// Calculate resolution time
+		log.Printf("[%d] %s in %v", c.StatusCode, c.Req.RequestURI, time.Since(t))
+	}
+}
---
+++ template/go.mod
@@ -0,0 +1,3 @@
+module github/Allen9012/gellen
+
+go 1.18
